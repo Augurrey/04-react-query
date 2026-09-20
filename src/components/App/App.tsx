e5dcd54b691efef8b1ace32152df56fade96f85d
Augurey <hameleon322@gmail.com>
2026-09-20 22:46:53 +0300

    fix pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,7 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 import ReactPaginateModule from "react-paginate";
 import type { ReactPaginateProps } from "react-paginate";
@@ -36,6 +36,7 @@ export default function App() {
     queryKey: ["movies", searchValue, page],
     queryFn: () => searchMovies(searchValue, page),
     enabled: Boolean(searchValue),
+    placeholderData: keepPreviousData,
   });
 
   useEffect(() => {
