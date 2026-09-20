b5a2b9e225648a828df71591f372504d82077048
Augurey <hameleon322@gmail.com>
2026-09-20 22:26:18 +0300

    disabled devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "modern-normalize";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./components/App/App.tsx";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +10,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
