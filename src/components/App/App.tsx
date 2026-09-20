9fdd1d54c7798f1f1637a927fa1b0b0a3b1bf73f
Augurey <hameleon322@gmail.com>
2026-09-20 22:23:38 +0300

    add pagination and tanstack query

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,53 +3,78 @@ import searchMovies from "../../services/movieService";
 import SearchBar from "../SearchBar/SearchBar";
 import toast, { Toaster } from "react-hot-toast";
 import type { Movie } from "../../types/movie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
+import { useQuery } from "@tanstack/react-query";
+
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
+import type { ComponentType } from "react";
+
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<
+    ComponentType<ReactPaginateProps>
+  >
+).default;
 
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isError, setIsError] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [page, setPage] = useState(1);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const handleSearch = async (query: string) => {
-    if (!query) {
-      setMovies([]);
-      setIsError(false);
-      return;
-    }
-
-    setIsLoading(true);
-    setIsError(false);
-    try {
-      const results = await searchMovies(query);
-      if (results.length === 0) {
-        toast.error("No movies found for your request.", {
-          duration: 2000,
-        });
-        setMovies([]);
-        return;
-      }
-      setMovies(results);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearch = (value: string) => {
+    if (value) setSearchValue(value);
+    setPage(1);
   };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["movies", searchValue, page],
+    queryFn: () => searchMovies(searchValue, page),
+    enabled: Boolean(searchValue),
+  });
+
+  useEffect(() => {
+    if (data && data.results.length === 0) {
+      toast.error("No movies found for your request.", {
+        duration: 2000,
+        position: "top-center",
+      });
+    }
+  }, [data]);
+
   return (
     <div className={styles.app}>
       <Toaster />
+
       <SearchBar onSubmit={handleSearch} />
+
       {isLoading && <Loader />}
-      {isError ? (
-        <ErrorMessage />
-      ) : (
-        <MovieGrid movies={movies} onSelect={setSelectedMovie} />
+
+      {isError && <ErrorMessage />}
+
+      {data && data.results.length > 0 && (
+        <>
+          <ReactPaginate
+            pageCount={data.total_pages}
+            pageRangeDisplayed={5}
+            marginPagesDisplayed={1}
+            onPageChange={({ selected }) => setPage(selected + 1)}
+            forcePage={page - 1}
+            containerClassName={styles.pagination}
+            activeClassName={styles.active}
+            nextLabel="→"
+            previousLabel="←"
+          />
+
+          <MovieGrid movies={data.results} onSelect={setSelectedMovie} />
+        </>
       )}
+
       {selectedMovie && (
         <MovieModal
           movie={selectedMovie}
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,16 +3,26 @@ import type { Movie } from "../types/movie";
 
 interface SearchMoviesResponse {
   results: Movie[];
+  total_pages: number;
+  page: number;
 }
 
 const token = import.meta.env.VITE_TMDB_TOKEN;
-const url = "https://api.themoviedb.org/3/search/movie?query=";
+const url = "https://api.themoviedb.org/3/search/movie";
 
-export default async function searchMovies(query: string): Promise<Movie[]> {
-  const response = await axios.get<SearchMoviesResponse>(`${url}${query}`, {
+export default async function searchMovies(
+  query: string,
+  page: number,
+): Promise<SearchMoviesResponse> {
+  const response = await axios.get<SearchMoviesResponse>(url, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
+    params: {
+      query,
+      page,
+    },
   });
-  return response.data.results;
+
+  return response.data;
 }
